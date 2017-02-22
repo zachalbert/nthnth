9c605533c7917546b992f8cdd9fccea22dec0fcd
zachalbert <zachalbert@gmail.com>
2017-02-21 17:14:16 -0800

    Add pretty URLs

--- gulpfile.js/tasks/markdown.js
+++ gulpfile.js/tasks/markdown.js
@@ -7,6 +7,7 @@ var gulp         = require('gulp')
 var markdown     = require('gulp-markdown')
 var marked       = require('marked')
 var handleErrors = require('../lib/handleErrors')
+var prettyURL    = require('gulp-pretty-url')
 var path         = require('path')
 var exclude      = path.normalize('!**/{' + config.tasks.markdown.excludeFiles.join(',') + '}')
 
@@ -25,6 +26,7 @@ var markdownTask = function() {
   return gulp.src(paths.src)
     .on('error', handleErrors)
     .pipe(markdown())
+    .pipe(prettyURL())
     .pipe(gulp.dest(paths.dest))
     .on('end', browserSync.reload)
 
