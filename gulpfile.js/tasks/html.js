5d8ad141a550743025ce57631f58e7e10a5522f8
zachalbert <zachalbert@gmail.com>
2017-02-28 09:00:36 -0800

    Fix some issues

--- gulpfile.js/tasks/html.js
+++ gulpfile.js/tasks/html.js
@@ -1,6 +1,9 @@
 var config       = require('../config')
 if(!config.tasks.html) return
 
+var cdata        = require('contentful-data')
+var entries      = null
+var util         = require('util')
 var browserSync  = require('browser-sync')
 var data         = require('gulp-data')
 var fs           = require('fs')
@@ -22,7 +25,63 @@ var getData = function(file) {
   return JSON.parse(fs.readFileSync(dataPath, 'utf8'))
 }
 
-var htmlTask = function() {
+gulp.task('cdata', function(cb){
+  // Only get entries once per build. Prevents numerous API calls if build run often after a gulp watch, for example.
+  if (!entries) {
+
+    var params = {
+      apiKey: '11074babe73772987909a59f76eb49bd6c847599d04181087892d6c1049ed1d7',
+      spaceId: 'g4u24h1d3en6',
+      opts: {
+        level: 2
+      }
+    };
+
+    return cdata(params, function(err, data){
+      if(!err) {
+        entries = data;
+        for( var x in entries ) {
+          console.log(util.inspect(x, {showHidden: true, depth: 3}));
+        }
+        cb();
+      } else {
+        console.error('error:' + err);
+      }
+    });
+  } else {
+    cb();
+  }
+})
+//
+// gulp.task('html', ['data'], function(cb){
+//   for (var pageId in entries.page){
+//
+//     // Assign nav and page data to template
+//     var locals = {
+//       navs: entries.navList,
+//       page: entries.page[pageId]
+//     }
+//
+//     // Get pug template to use.
+//     var template = entries.page[pageId].fields.template;
+//
+//     // Compile pug template
+//     var compiled = pug.compileFile('./src/templates/' + template + '.pug')(locals);
+//
+//     // Define path to output template
+//     var path = 'path/to/output/compiled/template';
+//     path = path + '/' + locals.page.fields.slug;
+//     mkpath.sync(path);
+//
+//     // Write compiled pug template
+//     var filename = path + '/index.html';
+//     fs.writeFileSync(filename,  compiled);
+//   }
+//
+//   cb();
+// });
+
+var htmlTask = function( entries ) {
 
   return gulp.src(paths.src)
     .pipe(data(getData))
@@ -30,7 +89,8 @@ var htmlTask = function() {
     .pipe(render({
       path: [path.join(config.root.src, config.tasks.html.src)],
       envOptions: {
-        watch: false
+        watch: false,
+        autoescape: false
       }
     }))
     .on('error', handleErrors)
@@ -40,5 +100,7 @@ var htmlTask = function() {
 
 }
 
-gulp.task('html', htmlTask)
+
+
+gulp.task('html', ['cdata'], htmlTask)
 module.exports = htmlTask
--- src/content/about.md
+++ src/content/about.md
@@ -9,9 +9,14 @@ description: "About us and stuff"
 [//]: # (Bleed image (-left, -right): <img src="http://www.placekitten.com/300/200" class="image image--bleed-left">)
 [//]: # (Text wrap image (-left, -right): <img src="http://www.placekitten.com/300/200" class="image float-left">)
 
-### About <img src="/images/magnet.svg" class="image svg" style="height: 20px;">
+### About
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel sem ac elit molestie eleifend consequat nec orci. Nam vitae nisi sollicitudin, bibendum justo quis, viverra turpis. Donec dui dui, tincidunt quis quam in, malesuada pulvinar mi.
+
+<div class="row">
+  <div class="col-xs-1" style="font-size: 16px;"><img src="/images/magnet.svg" class="image svg"></div>
+  <div class="col-xs-11"><h3>Subtitle</h3></div>
+</div>
 
 Suspendisse suscipit ligula nec mauris aliquam, sit amet congue ipsum lacinia. Sed fermentum iaculis commodo
 . Ut elementum ultrices mattis. Curabitur id odio metus. Aenean pellentesque justo in risus varius dictum. Aliquam sit amet elit a nisi fringilla pharetra. Proin nisi sapien, volutpat nec neque ut, accumsan placerat ligula. Proin eu nulla quis dolor malesuada efficitur sed vitae nunc. Aliquam varius arcu nec quam mattis, eu pharetra enim mattis. Nunc sit amet aliquet felis.
