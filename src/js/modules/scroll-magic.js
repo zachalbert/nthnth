e28bbe126a1f8d7f9a66c4510318bb681fcd3b6a
zachalbert <zachalbert@gmail.com>
2017-03-06 00:31:27 -0800

    Get mediaelement working

--- src/js/modules/scroll-magic.js
+++ src/js/modules/scroll-magic.js
@@ -4,13 +4,15 @@ import ScrollMagic from '../../../node_modules/scrollmagic/scrollmagic/minified/
 var controller = new ScrollMagic.Controller();
 var sidebarTrigger = $('.content-start').offset().top - $('.nav--top').outerHeight();
 
-var fixSidebar = new ScrollMagic.Scene({
-    offset: sidebarTrigger        // start this scene after scrolling for 50px
-    // triggerElement: "#sections" // thing to pin
-  })
-  .setPin(".sidebar--fixed"); // pins the element for the the scene's duration
+if( sidebarTrigger ) {
+  var fixSidebar = new ScrollMagic.Scene({
+      offset: sidebarTrigger        // start this scene after scrolling for 50px
+      // triggerElement: "#sections" // thing to pin
+    })
+    .setPin(".sidebar--fixed"); // pins the element for the the scene's duration
 
-// Add one or more scenes to the controller
-controller.addScene([
-  fixSidebar
-]);
+  // Add one or more scenes to the controller
+  controller.addScene([
+    fixSidebar
+  ]);
+}
