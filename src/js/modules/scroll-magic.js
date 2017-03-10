0725a06904c87445690b42ff1927a946f74bd364
zachalbert <zachalbert@gmail.com>
2017-03-09 22:53:13 -0800

    Fix scroll magic bug on pages without sidebar

--- src/js/modules/scroll-magic.js
+++ src/js/modules/scroll-magic.js
@@ -1,14 +1,20 @@
-import ScrollMagic from '../../../node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js';
+import ScrollMagic from '../../../node_modules/scrollmagic/';
 
 // init controller
-var controller = new ScrollMagic.Controller();
-var sidebarTrigger = $('.content-start').offset().top - $('.nav--top').outerHeight();
+var controller       = new ScrollMagic.Controller();
+var trigger          = $('.fixed-scroll-offset');
+
+if( trigger.length ) {
+  var sidebarTrigger = trigger.offset().top - $('.nav--top').outerHeight();
+}
 
 if( sidebarTrigger ) {
   var fixSidebar = new ScrollMagic.Scene({
       offset: sidebarTrigger        // start this scene once sidebarTrigger has been reached
     })
-    .setPin(".sidebar--fixed");     // pins the element for the the scene's duration
+    .setPin(".sidebar--fixed", {
+      pushFollowers: false
+    });     // pins the element for the the scene's duration
 
   // Add one or more scenes to the controller
   controller.addScene([
