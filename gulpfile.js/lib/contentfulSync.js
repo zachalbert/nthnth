1a34707a746212bf443196b97bf8d7e4070930f1
Thomas Rampelberg <thomas@saunter.org>
2017-03-14 15:53:17 -0700

    Super minor fix, still need to resolve links

--- gulpfile.js/lib/contentfulSync.js
+++ gulpfile.js/lib/contentfulSync.js
@@ -1,7 +1,10 @@
+var _                 = require('lodash')
 var Promise           = require("bluebird")
 var config            = require('config')
 var contentful        = require('contentful-management')
 var memoize           = require('memoizee')
+
+var locale = config.get("contentful.locale")
 
 // Get some userfriendly names out of the types (can sometimes be ids)
 var getTypeNames = function(result) {
@@ -11,18 +14,27 @@ var getTypeNames = function(result) {
   }, {})
 }
 
+var isLink = function(item) {
+  return _.has(item[locale], 'sys')
+}
+
 // Produce a data set that has all the items to be rendered:
 // {
 //   "entry_name": [ fields, fields]
 // }
 var buildData = function(result) {
+
   var typeNames = getTypeNames(result)
   return result.entries.items.reduce(function(acc, val) {
     var name = typeNames[val.sys.contentType.sys.id]
 
     if (!(name in acc)) { acc[name] = [] }
 
-    acc[name].push(val.fields)
+    var fields = Object.entries(val.fields).reduce(function(acc, [k, v]) {
+        return Object.assign(acc, { [k]: v[locale] })
+      }, {})
+
+    acc[name].push(fields)
     return acc
   }, {})
 }
