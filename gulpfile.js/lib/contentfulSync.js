fe801e53abe44679c8b11fcf14893bc58cba5b46
Thomas Rampelberg <thomas@saunter.org>
2017-03-15 10:27:33 -0700

    Fixing the move to management api

--- gulpfile.js/lib/contentfulSync.js
+++ gulpfile.js/lib/contentfulSync.js
@@ -14,42 +14,64 @@ var getTypeNames = function(result) {
   }, {})
 }
 
-var isLink = function(item) {
-  return _.has(item[locale], 'sys')
+var isEntry = function(item) {
+  return _.has(item, 'sys')
 }
+
+var handleEntry = function(space, entry) {
+  return Promise.reduce(Object.entries(entry.fields),
+    function(acc, [name, field]) {
+      var val = field[locale];
+      if (isEntry(val)) {
+        return space[`get${val.sys.linkType}`](val.sys.id).then(
+          _.curry(handleEntry)(space)).then(function(entry) {
+            return Object.assign(acc, { [name]: entry })
+          })
+      }
+
+      return Object.assign(acc, { [name]: val })
+    }, {}).catch(function() {
+      console.log(arguments)
+    })
+}
+
 
 // Produce a data set that has all the items to be rendered:
 // {
 //   "entry_name": [ fields, fields]
 // }
-var buildData = function(result) {
+var buildData = function(space, result) {
 
   var typeNames = getTypeNames(result)
-  return result.entries.items.reduce(function(acc, val) {
-    var name = typeNames[val.sys.contentType.sys.id]
+  return Promise.map(result.entries.items, function(item) {
+    var name = typeNames[item.sys.contentType.sys.id]
 
-    if (!(name in acc)) { acc[name] = [] }
-
-    var fields = Object.entries(val.fields).reduce(function(acc, [k, v]) {
-        return Object.assign(acc, { [k]: v[locale] })
-      }, {})
-
-    acc[name].push(fields)
-    return acc
-  }, {})
+    return handleEntry(space, item).then(function(entry) {
+      return [name, entry]
+    })
+  })
 }
 
 var getData = memoize(function(cb) {
   var client = contentful.createClient({
-    "accessToken": config.get("contentful.manageToken")
+    "accessToken": config.get("contentful.manageToken"),
+    "resolveLinks": true
   })
 
   client.getSpace(config.get("contentful.space")).then(function(space) {
+
     Promise.props({
       "entries": space.getEntries(),
       "types": space.getContentTypes()
     }).then(function(result) {
-      cb(undefined, { contentful: buildData(result) })})
+      buildData(space, result).then(function(data) {
+        var actual = data.reduce(function(acc, [name, val]) {
+          acc[name] = _.get(acc, name, []).concat([val])
+          return acc
+        }, {})
+        cb(undefined, { contentful: actual })
+      })
+    })
   })
 
 }, {
